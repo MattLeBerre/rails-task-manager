56775b74a1a5bd911ca908badfd92f0ef6f3850e
Matthieu Le Berre <matthieu.leberre@gmail.com>
2016-11-22 15:47:22 +0100

    ok

--- config/routes.rb
+++ config/routes.rb
@@ -3,15 +3,15 @@ Rails.application.routes.draw do
 
   get 'tasks/:id', to: 'tasks#show', as: 'task'
 
-  get 'tasks/new'
+  get 'new', to: 'tasks#new'
+  post 'tasks', to: 'tasks#create'
 
-  get 'tasks/create'
-
-  get 'tasks/edit'
+  get 'tasks/:id/edit', to: 'tasks#edit', as: "edit_task"
+  patch 'tasks/:id' , to: 'tasks#update'
 
   get 'tasks/update'
 
-  get 'tasks/destroy'
+  delete 'tasks/:id', to: 'tasks#destroy'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
